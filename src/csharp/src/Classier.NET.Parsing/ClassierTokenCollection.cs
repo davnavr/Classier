7ba2b725a7c6ef9f2be9dc2f6ee3b674569fb688
davnavr <davidcalieln@gmail.com>
2020-02-16 21:56:38 -0800

    Added collection for tokens used in the Classier language, and an enum to represent the type of the tokens

---
+++ src/csharp/src/Classier.NET.Parsing/ClassierTokenCollection.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Copyright (c) 2020, David Navarro. All rights reserved.
+ * Licensed under the MIT license. For more information, see the 'LICENSE' file in the project root.
+ */
+
+namespace Classier.NET.Parsing
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a collection of the tokens in the Classier language.
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class ClassierTokenCollection : IEnumerable<ITokenDefinition>
+    {
+        /// <inheritdoc/>
+        public IEnumerator<ITokenDefinition> GetEnumerator()
+        {
+            ////yield return new
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ src/csharp/src/Classier.NET.Parsing/ClassierTokenType.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright (c) 2020, David Navarro. All rights reserved.
+ * Licensed under the MIT license. For more information, see the 'LICENSE' file in the project root.
+ */
+
+namespace Classier.NET.Parsing
+{
+    /// <summary>
+    /// Represents the type of a token in the Classier language.
+    /// </summary>
+    public enum ClassierTokenType
+    {
+        /// <summary>
+        /// The token is of an unknown type.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// The token is a closing parenthesis.
+        /// </summary>
+        CloseParen,
+
+        /// <summary>
+        /// The token is an integer literal.
+        /// </summary>
+        IntegerLiteral,
+
+        /// <summary>
+        /// The token is a opening parenthesis.
+        /// </summary>
+        OpenParen,
+
+        /// <summary>
+        /// The token indicates the end of a statement (a semicolon).
+        /// </summary>
+        StatementEnd,
+
+        /// <summary>
+        /// The token is a string.
+        /// </summary>
+        StringLiteral,
+    }
+}
