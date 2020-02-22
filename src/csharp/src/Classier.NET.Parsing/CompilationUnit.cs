ac99dbaf9b4ef8118b7124d0198a75d01220c8bd
davnavr <davidcalieln@gmail.com>
2020-02-21 21:09:23 -0800

    Removed obsolete classes. Added documentation comments to the constructors of the 'CompilationUnit' class

--- src/csharp/src/Classier.NET.Parsing/CompilationUnit.cs
+++ src/csharp/src/Classier.NET.Parsing/CompilationUnit.cs
@@ -10,29 +10,46 @@ namespace Classier.NET.Parsing
     using System.Linq;
 
     /// <summary>
-    /// Represents a 
+    /// Represents a single Classier source code file.
     /// </summary>
-    public class CompilationUnit
+    public class CompilationUnit // TODO: Rename to 'SourceFile'?
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from the tokens.
+        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from the specfiied tokens.
         /// </summary>
-        /// <param name="tokens"></param>
+        /// <param name="tokens">The tokens in the source code file.</param>
         public CompilationUnit(IEnumerable<Token> tokens)
+            : this(tokens, new string[0]) // TODO: Is making an empty array faster?
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from the specfiied tokens and conditional compilation symbols.
+        /// </summary>
+        /// <param name="tokens">The tokens in the source code file.</param>
+        /// <param name="symbols">An array containing the conditional compilation symbols.</param>
         public CompilationUnit(IEnumerable<Token> tokens, params string[] symbols)
             : this(tokens, symbols.AsEnumerable())
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from the tokens and the conditional compilation symbols.
+        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from the specfiied tokens and conditional compilation symbols.
         /// </summary>
-        /// <param name="tokens"></param>
+        /// <param name="tokens">The tokens in the source code file.</param>
         /// <param name="symbols">A collection containing the conditional compilation symbols.</param>
-        public CompilationUnit(IEnumerable<Token> tokens, IEnumerable<string> symbols) // TODO: Make enumerator class that wraps an enumerator for tokens and skips whitespace tokens.
+        public CompilationUnit(IEnumerable<Token> tokens, IEnumerable<string> symbols) // TODO: Make enumerator class that wraps an enumerator for tokens and skips whitespace tokens and has other functionality.
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompilationUnit"/> class from a file with the specified encoding and conditional compilation symbols.
+        /// </summary>
+        /// <param name="path">The path to the source code file.</param>
+        /// <param name="encoding">The encoding of the source code file.</param>
+        /// <param name="symbols">A collection containing the conditional compilation symbols.</param>
+        public CompilationUnit(string path, System.Text.Encoding encoding, IEnumerable<string> symbols)
+            : this(new TextTokenCollection(() => new System.IO.StreamReader(path, encoding)), symbols)
         {
         }
     }
