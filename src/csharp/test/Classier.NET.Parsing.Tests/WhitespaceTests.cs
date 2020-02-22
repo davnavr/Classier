83aad5dc1da0804d70504479d5940860c28e5c98
davnavr <davidcalieln@gmail.com>
2020-02-22 12:22:29 -0800

    Added tests for the 'Whitespace' class

---
+++ src/csharp/test/Classier.NET.Parsing.Tests/WhitespaceTests.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright (c) 2020, David Navarro. All rights reserved.
+ * Licensed under the MIT license. For more information, see the 'LICENSE' file in the project root.
+ */
+
+namespace Classier.NET.Parsing.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Xunit;
+
+    public class WhitespaceTests
+    {
+        [Theory]
+        [InlineData("    ", 1)]
+        [InlineData("  \n  ", 2)]
+        [InlineData("    \t ", 3)]
+        public void WhitespaceFromTokensIsValid(string text, int tokenCount)
+        {
+            // Arrange
+            var tokens = new TextTokenCollection(() => new StringReader(text));
+
+            // Act
+            var ws = new Whitespace(tokens);
+
+            // Assert
+            Assert.Equal(tokenCount, ws.Count);
+            Assert.Equal(text, ws.ToString());
+        }
+    }
+}
