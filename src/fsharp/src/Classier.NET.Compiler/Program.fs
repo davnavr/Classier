df070c0e858465b21c95be93842e5476b7247bad
davnavr <davidcalieln@gmail.com>
2020-03-19 21:52:20 -0700

    Added soluition and F# project files

---
+++ src/fsharp/src/Classier.NET.Compiler/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module Classier.NET.Compiler.CommandLine
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
