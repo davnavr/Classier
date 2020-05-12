534d500b1e30365033452220b4185c9620d9a8c5
davnavr <davidcalieln@gmail.com>
2020-05-11 20:53:48 -0700

    While loops, variables, statements

--- src/Classier.NET.Compiler/Parsing/Grammar.fs
+++ src/Classier.NET.Compiler/Parsing/Grammar.fs
@@ -52,7 +52,6 @@ type NumLiteral<'Type> =
     { Base: byte
       FracPart: char list
       IntPart: char list
-      IsNegative: bool
       Type: 'Type }
 
 type Definition =
@@ -62,6 +61,7 @@ type Definition =
 
 type TypeName =
     | Identifier of Identifier list
+    | Inferred
     | Tuple of TypeName list
 and Identifier =
     { Name: string
@@ -69,24 +69,45 @@ and Identifier =
 and GenericArg = TypeName
 
 type Expression =
-    | FuncCall
+    | FloatLit of NumLiteral<FloatType>
+    | FuncCall of
+        {| Arguments: Expression list list
+           Target: Expression |}
+    | IntLit of NumLiteral<IntType>
     | MemberAccess of Identifier list
     | Nested of Expression
-    | IntLiteral of NumLiteral<IntType>
-    | FloatLiteral of NumLiteral<FloatType>
+    | TupleLit of Expression list
+    | UnitLit
 
 type Variable =
     { Definition: Definition
-      Type: TypeName option
+      Type: TypeName
       Value: Expression option }
+
+
+type Statement =
+    | Empty
+    | LocalVar of Variable
+    | Return of Expression
+    | While of Expression * Statement list
+
+type Param =
+    { Name: string
+      Type: TypeName }
+
+type Function =
+    { Body: Statement list
+      Definition: Definition
+      Parameters: Param list list
+      ReturnType: TypeName }
 
 type TypeHeader =
     | Class of
         {| SuperClass: TypeName option |}
+    | DUnion
     | Interface
     | Module
     | Record
-    | Union
 and TypeDef =
     { Definition: Definition
       Header: TypeHeader
@@ -94,9 +115,9 @@ and TypeDef =
       Members: MemberDef list }
 and MemberDef =
     | Field of Variable
-    | Function
+    | Function of Function
     | NestedType of TypeDef
-    | DUnionCase
+    | DUnionCase of string * TypeName option
 
 type CompilationUnit =
     { Definitions: TypeDef list
@@ -125,12 +146,12 @@ let parser: Parser<CompilationUnit, ParserState> =
     let colon = skipChar ':'
     let comma = skipChar ','
     let gtsign = skipChar '>'
-    let lcurlybracket = skipChar '{'
+    let lcurlybracket = skipChar '{' <?> "opening bracket"
     let lparen = skipChar '(' <?> "opening parenthesis"
     let ltsign = skipChar '<'
     let opequal = skipChar '='
     let period = skipChar '.'
-    let rcurlybracket = skipChar '}'
+    let rcurlybracket = skipChar '}' <?> "closing bracket"
     let rparen = skipChar ')' <?> "closing parenthesis"
     let semicolon = skipChar ';' <?> "semicolon"
 
@@ -151,6 +172,7 @@ let parser: Parser<CompilationUnit, ParserState> =
 
     let identifier, identifierRef = createParserForwardedToRef<Identifier, _>()
     let typeName, typeNameRef = createParserForwardedToRef<TypeName, _>()
+    let statementBlock, statementBlockRef = createParserForwardedToRef<Statement list, _>()
 
     let ignored =
         choice
@@ -172,7 +194,6 @@ let parser: Parser<CompilationUnit, ParserState> =
             ]
         |> attempt
         |> skipMany
-
     let ignored1 = notEmpty ignored
 
     let typeAnnotation =
@@ -181,14 +202,21 @@ let parser: Parser<CompilationUnit, ParserState> =
         |> attempt
         >>. ignored
         >>. typeName
-        |> opt
         <?> "type annotation"
+
+    let typeAnnotationOpt =
+        typeAnnotation
+        |> opt
+        |>> Option.defaultValue Inferred
 
     let modifier word flags =
         skipString word
-        |> optional
-        .>> ignored
+        |> attempt
+        .>> ignored1
         .>> setFlags flags
+        |> optional
+    let modifierAbstract = modifier "abstract" Flags.Abstract
+    let modifierMutable = modifier "mutable" Flags.Mutable
 
     let separator p = ignored >>. p .>> ignored
 
@@ -202,7 +230,10 @@ let parser: Parser<CompilationUnit, ParserState> =
         .>> rcurlybracket
 
     let genericArgs =
-        sepBy1 typeName (separator comma)
+        ltsign
+        |> attempt
+        >>. sepBy1 typeName (separator comma)
+        .>> gtsign
         |> opt
         |>> Option.defaultValue []
         <?> "generic arguments"
@@ -221,24 +252,52 @@ let parser: Parser<CompilationUnit, ParserState> =
     let expression =
         let decimalChars = [ '0'..'9' ]
         let expr = OperatorPrecedenceParser<_,_,_> ()
+        let exprParser = expr.ExpressionParser <?> "expression"
+
+        let mapOperator name expr1 expr2 =
+            FuncCall {| Arguments = [ [ expr2 ] ]; Target = expr1 |} // The target should be the method on expr1.
+
+        // Mathematical operators
+        [
+            "add", '+', 10, Associativity.Left
+            "add", '-', 10, Associativity.Left
+            "multiply", '*', 20, Associativity.Left
+            "multiply", '/', 20, Associativity.Left
+        ]
+        |> List.map (fun (name, op, prec, assoc) -> new InfixOperator<_,_,_>(string op, ignored, prec, assoc, mapOperator name))
+        |> List.iter expr.AddOperator
+
+        expr.AddOperator (PrefixOperator<_,_,_>("-", ignored, 50, true, fun exp -> FuncCall {| Arguments = []; Target = exp (* = "negate"*) |}))
+
+        let tuple =
+            lparen
+            |> attempt
+            >>. ignored
+            >>. sepBy exprParser (separator comma)
+            .>> rparen
 
         expr.TermParser <-
             choice
                 [
                     identifierList |>> MemberAccess;
-                    
-                    opt (pchar '-')
-                    .>>.
-                        (pchar '0'
-                        |> attempt
-                        >>. choice
-                            [
-                                anyOf [ 'b'; 'B' ] >>. preturn [ '0'; '1'; ];
-                                anyOf [ 'x'; 'X' ] >>. preturn (decimalChars @ [ 'a'..'z' ] @ [ 'A'..'Z' ]);
-                            ]
-                        <|> preturn decimalChars
-                        <|> (pchar '.' >>. preturn []))
-                    >>= (fun (neg, chars) ->
+
+                    tuple
+                    |>> (fun ex ->
+                        match ex with
+                        | [] -> UnitLit
+                        | [ nested ] -> nested
+                        | _ -> TupleLit ex);
+
+                    pchar '0'
+                    >>. choice
+                        [
+                            anyOf [ 'b'; 'B' ] >>. preturn [ '0'; '1'; ];
+                            anyOf [ 'x'; 'X' ] >>. preturn (decimalChars @ [ 'a'..'z' ] @ [ 'A'..'Z' ]);
+                        ]
+                    |> attempt
+                    <|> preturn decimalChars
+                    <|> (pchar '.' >>. preturn [])
+                    >>= (fun chars ->
                         let digits c =
                             attempt (anyOf c) .>> (skipMany (pchar '_') <?> "digit separator") |> many1
                         let decimalDigits = digits decimalChars
@@ -272,11 +331,10 @@ let parser: Parser<CompilationUnit, ParserState> =
                                     ]
                                     IntType.Integer
                                 |>> fun intType ->
-                                    IntLiteral
+                                    IntLit
                                         { Base = byte(chars.Length)
                                           FracPart = []
                                           IntPart = idigits
-                                          IsNegative = neg.IsSome
                                           Type = intType }
                             else
                                 suffixes
@@ -287,23 +345,33 @@ let parser: Parser<CompilationUnit, ParserState> =
                                     ]
                                     FloatType.Double
                                 |>> fun floatType ->
-                                    FloatLiteral
+                                    FloatLit
                                         { Base = byte(10)
                                           FracPart = fdigits
                                           IntPart = idigits
-                                          IsNegative = neg.IsSome
                                           Type = floatType })
                     <?> "numeric literal";
                 ]
+                .>> ignored
+                .>>. (tuple .>> ignored |> many <?> "argument list")
+                |>> fun (target, args) ->
+                    match args with
+                    | [] -> target
+                    | _ ->
+                        FuncCall
+                            {| Arguments = args
+                               Target = target |}
 
-        expr.ExpressionParser <?> "expression"
+        exprParser
 
     let variableDef =
         setUserState { ParserState.Default with Visibility = Visibility.Private }
         .>> skipString "let"
+        |> attempt
         .>> ignored1
+        .>> modifierMutable
         >>. identifierStr
-        .>>. typeAnnotation
+        .>>. typeAnnotationOpt
         .>> ignored
         .>>. opt
             (opequal
@@ -323,14 +391,17 @@ let parser: Parser<CompilationUnit, ParserState> =
     typeNameRef :=
         choice
             [
-                identifierFull
+                identifierFull;
+
+                skipChar '_'
+                >>. preturn Inferred;
 
                 between
                     lparen
                     rparen
                     (sepBy typeName (separator comma))
                 |>> Tuple
-                <?> "tuple"
+                <?> "tuple";
             ]
         <?> "type name"
 
@@ -341,6 +412,40 @@ let parser: Parser<CompilationUnit, ParserState> =
         |>> fun (name, gparams) ->
             { Name = name
               GenericArgs = gparams }
+
+    statementBlockRef :=
+        [
+            semicolon
+            >>. preturn Empty
+            <?> "empty statement";
+
+            variableDef
+            |>> LocalVar
+            <?> "local variable";
+
+            skipString "while"
+            |> attempt
+            >>. ignored
+            >>. lparen
+            >>. expression
+            .>> rparen
+            .>> ignored
+            .>>. statementBlock
+            |>> While
+            <?> "while loop";
+
+            skipString "return"
+            |> attempt
+            .>> ignored1
+            |> optional
+            >>. expression
+            |> attempt
+            |>> Return
+            <?> "return statement"
+            .>> ignored
+            .>> semicolon; // NOTE: Use >>= to check if the expression needs a semicolon.
+        ]
+        |> block
     
     let nsStatementL word label =
         pstring word
@@ -372,6 +477,39 @@ let parser: Parser<CompilationUnit, ParserState> =
         |>> Field
         <?> "field definition"
 
+    let paramTuple =
+        lparen
+        .>> ignored
+        >>. sepBy
+                (identifierStr
+                .>>. typeAnnotationOpt
+                |>> fun (name, ptype) ->
+                    { Name = name
+                      Type = ptype })
+                (separator comma)
+        .>> rparen
+        <?> "parameters"
+
+    let functionDef =
+        getUserState
+        .>>. identifierStr
+        .>> ignored
+        .>>. genericParams
+        .>> ignored
+        .>>. (paramTuple .>> ignored |> many1 <?> "parameter list")
+        .>>. typeAnnotationOpt
+        .>> ignored
+        .>>. (semicolon >>. preturn [] <|> statementBlock)
+        |>> fun (((((st, name), gparams), fparams), retType), body) ->
+            Function
+                { Body = body
+                  Definition =
+                    { Name = name
+                      Flags = st.Flags
+                      Visibility = st.Visibility }
+                  Parameters = fparams
+                  ReturnType = retType }
+
     let implements =
         ignored1
         .>> skipString "implements"
@@ -381,6 +519,70 @@ let parser: Parser<CompilationUnit, ParserState> =
         |> opt
         |>> Option.defaultValue []
         <?> "interface implementations"
+
+    let recordDef =
+        typeDef "data"
+        .>>. identifierStr
+        .>>. genericParams
+        .>> ignored
+        .>>. block
+            [
+                identifierStr
+                .>>. typeAnnotation
+                .>> ignored
+                .>> semicolon
+                <?> "record field"
+                |>> fun (name, ftype) ->
+                    Field
+                        { Definition =
+                            { Name = name
+                              Flags = Flags.None
+                              Visibility = Visibility.Public }
+                          Value = None
+                          Type = ftype }
+
+                accessModifier Visibility.Private
+                >>. ignored1
+                >>. modifier "inline" Flags.Inline
+                >>. functionDef
+                <?> "method definition";
+            ]
+        <?> "record definition"
+        |>> fun (((st, name), gparams), members) ->
+            { Definition =
+                { Name = name
+                  Flags = st.Flags
+                  Visibility = st.Visibility }
+              Header = Record
+              Interfaces = []
+              Members = members }
+
+    let unionDef =
+        typeDef "union"
+        .>>. identifierStr
+        .>>. genericParams
+        .>> ignored
+        .>>. block
+            [
+                identifierStr
+                .>>. typeAnnotationOpt
+                .>> ignored
+                .>> semicolon
+                <?> "union case"
+                |>> fun (name, ctype) ->
+                    match ctype with
+                    | Inferred -> DUnionCase (name, None)
+                    | _ -> DUnionCase (name, Some ctype)
+            ]
+        <?> "discriminated union"
+        |>> fun (((st, name), gparams), cases) ->
+            { Definition =
+                { Name = name
+                  Flags = st.Flags
+                  Visibility = st.Visibility }
+              Header = DUnion
+              Interfaces = []
+              Members = cases }
 
     let classDef =
         typeDef "class"
@@ -396,7 +598,15 @@ let parser: Parser<CompilationUnit, ParserState> =
         .>> ignored
         .>>. block
             [
-                fieldDef
+                fieldDef;
+
+                accessModifier Visibility.Private
+                >>. ignored1
+                >>. modifierAbstract
+                >>. modifier "inline" Flags.Inline
+                >>. modifier "mutator" Flags.Mutable
+                >>. functionDef
+                <?> "method definition";
             ]
         <?> "class definition"
         |>> fun (((((state, name), gparams), superclass), iimpls), members) ->
@@ -415,7 +625,17 @@ let parser: Parser<CompilationUnit, ParserState> =
         .>> ignored
         .>>. block
             [
-                fieldDef
+                fieldDef;
+                
+                accessModifier Visibility.Private
+                >>. ignored1
+                >>. modifier "inline" Flags.Inline
+                >>. choice
+                    [
+                        recordDef |>> NestedType
+                        unionDef |>> NestedType
+                        functionDef <?> "function definition"
+                    ]
             ]
         <?> "module definition"
         |>> fun (((state, name), gparams), members) ->
@@ -431,18 +651,22 @@ let parser: Parser<CompilationUnit, ParserState> =
         accessModifier Visibility.Internal
         >>. ignored1
         >>. modifier "inheritable" Flags.Inheritable // TODO: use >>= to check that these modifiers are only applied on classes.
-        >>. modifier "abstract" Flags.Abstract
+        >>. modifierAbstract
+        >>. modifierMutable
         >>. choice
             [
                 classDef
                 moduleDef
+                recordDef
+                unionDef
             ]
 
     ignored
     >>. opt (nsStatementL "namespace" "namespace declaration")
     .>> ignored
     .>>. many (nsStatementL "use" "use statement" .>> ignored)
-    .>>. sepBy1 typeDefs ignored
+    .>>. many1 (typeDefs .>> ignored)
+    .>> eof
     |>> fun ((ns, uses), defs) ->
         { Definitions = defs
           Namespace = Option.defaultValue [] ns
