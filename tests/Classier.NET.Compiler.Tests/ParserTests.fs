b25f5b59f4f7e04cf0fd4ae35693fdbd848e7742
davnavr <davidcalieln@gmail.com>
2020-06-14 14:47:54 -0700

    Switched from C# xunit tests to F# Fuchu tests

---
+++ tests/Classier.NET.Compiler.Tests/ParserTests.fs
@@ -0,0 +1,12 @@
+﻿module Classier.NET.Compiler.ParserTests
+
+open Fuchu
+
+[<Tests>]
+let tests =
+    [
+        test "this is a test" {
+            Assert.Equal("test message", 4, 4);
+        }
+    ]
+    |> testList ""
