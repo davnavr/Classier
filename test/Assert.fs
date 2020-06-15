fc429a12dea72547789ce4a7970562964169c6bd
davnavr <davidcalieln@gmail.com>
2020-06-14 22:29:36 -0700

    Added some unit tests back to check if it will run

---
+++ test/Assert.fs
@@ -0,0 +1,22 @@
+﻿module Classier.NET.Compiler.Assert
+
+open FParsec
+open Fuchu
+
+let testSuccess name test result =
+    fun _ ->
+        match result with
+        | Success (success, state, _) -> test (success, state)
+        | Failure (msg, _ ,_) -> raise (AssertException msg)
+    |> testCase name 
+
+let equal expected actual =
+    let msg =
+        sprintf
+            "%A should be equal to %A"
+            actual
+            expected
+
+    Assert.Equal(msg, expected, actual)
+
+let testsOfResult tests result = Seq.map (fun (test: 'a -> Test) -> test result) tests
---
+++ test/Tests.fs
@@ -0,0 +1,50 @@
+﻿module Classier.NET.Compiler.Tests
+
+open System.Reflection
+open Classier.NET.Compiler.Assert
+open FParsec
+open Fuchu
+
+let parseSource source =
+    use file =
+        source
+        |> sprintf "Classier.NET.Compiler.%s.txt"
+        |> Assembly.GetExecutingAssembly().GetManifestResourceStream
+
+    runParserOnStream
+        Parser.compilationUnit
+        ParserState.defaultState
+        source
+        file
+        System.Text.Encoding.UTF8
+
+[<EntryPoint>]
+let main args =
+    [
+        [
+            "MultipleClasses", [ "test" ], List.empty
+            "MyAbstractClass", [ "this"; "is"; "my"; "space" ], [ [ "java"; "lang" ]; [ "java"; "util" ] ]
+        ]
+        |> Seq.map
+            (fun (sourceName, ns, usings) ->
+                parseSource sourceName
+                |> testsOfResult
+                    [
+                        testSuccess
+                            "correct namespace"
+                            (fun (cu, _) -> equal (Identifier.ofStrings ns) cu.Namespace)
+
+                        testSuccess
+                            "correct usings"
+                            (fun (cu, _) ->
+                                let useList =
+                                    usings
+                                    |> Seq.map (Identifier.ofStrings >> Option.get)
+                                    |> List.ofSeq
+                                cu.Usings |> equal useList)
+                    ])
+        |> Seq.collect id
+        |> testList "success tests"
+    ]
+    |> testList "parser tests"
+    |> run
