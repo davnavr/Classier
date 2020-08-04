4c8c43fb78099dadc41322f4969999d3e5daa5f4
davnavr <davidcalieln@gmail.com>
2020-08-04 14:11:08 -0700

    Program now uses analyzer and uses functions in Print module to write output

--- src/Program.fs
+++ src/Program.fs
@@ -2,25 +2,34 @@
 
 open System
 open System.IO
-open Classier.NET.Compiler.ToSource
+
+open Classier.NET.Compiler
+open Classier.NET.Compiler.SemAnalysis
 
 /// The entry point of the compiler.
 [<EntryPoint>]
-let main args = // TODO: Handle arguments correctly. Need to have an argument that specifies the output.
+let main args = // TODO: Handle arguments correctly. Need to have an argument that specifies the output file.
     let parse =
         args
         |> Array.takeWhile (fun arg -> arg.StartsWith "--" |> not)
         |> Seq.map Path.GetFullPath
         |> Parser.parseFiles System.Text.Encoding.UTF8
-
     match parse() with
     | Result.Ok (cunits, epoint) ->
-        use fileout = new StreamWriter(File.OpenWrite (Array.last args))
-        let output =
-            Output.write
-                { Definitions = System.Collections.Immutable.ImmutableList.Empty
-                  EntryPoint = epoint }
+        let analysis = Analyze.output (cunits, epoint) GlobalsTable.empty // TODO: How to allow additions of extern types to the globals table?
+        match analysis with
+        | Result.Ok output ->
+            use fileout = new StreamWriter(File.OpenWrite (Array.last args))
+            Print.it
                 fileout.WriteLine
-        output()
-    | Result.Error err -> Console.Error.Write err
-    0
+                (Print.poutput output 4)
+            0
+        | Result.Error errors ->
+            for err in errors do
+                err
+                |> AnalyzerError.print
+                |> stderr.WriteLine
+            -1
+    | Result.Error err ->
+        stderr.WriteLine err
+        -1
